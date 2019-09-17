ea672bc78b599353aa932bede74a61b1b21ddf4d
Doug Kintop <dkintop@hotmail.com>
2019-09-17 16:47:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
   
-def initialize(title, author, page_count)
+  def initialize(title, author, page_count)
   
-  @title = title
-  @author = author
-  @page_count = page_count
+    @title = title
+    @author = author
+    @page_count = page_count
+  
+  end
 
 end
 
